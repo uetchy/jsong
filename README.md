5653341de30c7c6e811a6f6e829a99877939a711
Yasuaki Uechi <y@uechi.io>
2019-04-24 13:49:45 +0900

    add base

---
+++ README.md
@@ -0,0 +1,7 @@
+# jsong (JSON Google)
+
+> Search on Google and get the result in JSON.
+
+```
+curl -sL https://jsong.now.sh?query=GitHub | jq '.links'
+```
---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const helmet = require('helmet')
+const google = require('google')
+const app = express()
+
+app.use(helmet())
+
+app.get('*', async (req, res) => {
+  const query = req.query.query || req.query.q
+  if (!query) {
+    return res.json({ error: 'no query' })
+  }
+  const params = {
+    tld: req.query.tld || 'com',
+    lang: req.query.lang || 'en',
+  }
+  const links = await googlePromise(query, params)
+  res.json({ params: { query, ...params }, links })
+})
+
+function googlePromise(query, { tld, lang }) {
+  return new Promise((resolve, reject) => {
+    google.tld = tld
+    google.lang = lang
+    google.resultsPerPage = 25
+    google(query, (err, res) => {
+      if (err) reject(err)
+      resolve(res.links)
+    })
+  })
+}
+
+module.exports = app
